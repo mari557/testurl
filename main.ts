e75df3510fe6ec64b2934f548d2f02b2c48c3557
mari557 <111214150+mari557@users.noreply.github.com>
2026-02-05 10:18:57 +0900

    Update main.ts

--- main.ts
+++ main.ts
@@ -5,7 +5,7 @@ Deno.serve(async (req: Request) => {
   const encodedUrl = url.searchParams.get("url");
 
   if (!encodedUrl) {
-    return new Response("사용법: ?url=" + Buffer.from("https://mail.naver.com").toString("base64"));
+    return new Response("사용법: ?url=" + Buffer.from("https://mwww.upbit.com).toString("base64"));
   }
 
   try {
